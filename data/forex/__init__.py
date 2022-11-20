d7ad2fa15443ee016b9c17a26c4482b595ef78ff
Andrew Stepura <andstepura@gmail.com>
2022-11-19 20:34:02 -0500

    Added Forex feature engineering

--- data/forex/__init__.py
+++ data/forex/__init__.py
@@ -3,8 +3,12 @@ from ._loading import (
     load_processed_forex_data,
     load_raw_forex_data
 )
-from._preprocessing import (
+from ._preprocessing import (
     preprocess_forex_data
+)
+from ._feature_engineering import (
+    ForexFeEngStrategy,
+    engineer_forex_features
 )
 
 __all__ = [
@@ -13,5 +17,7 @@ __all__ = [
     'ForexDataSource',
     'load_processed_forex_data',
     'load_raw_forex_data',
-    'preprocess_forex_data'
+    'preprocess_forex_data',
+    'ForexFeEngStrategy',
+    'engineer_forex_features'
 ]
---
+++ data/forex/_feature_engineering.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from typing import Dict
+from enum import Enum
+from ._common import FOREX_COLS
+
+class ForexFeEngStrategy(Enum):
+    STRATEGY1 = 0 # "Financial Trading as a Game: A Deep Reinforcement Learning Approach" 
+
+
+def engineer_forex_features(
+    data: Dict[str, pd.DataFrame],
+    strategy: ForexFeEngStrategy,
+    strategy_params: Dict
+) -> pd.DataFrame:
+    if strategy == ForexFeEngStrategy.STRATEGY1:
+        return _engineer_forex_features_strategy1(data, **strategy_params)
+    
+
+def _engineer_forex_features_strategy1(
+    data: Dict[str, pd.DataFrame],
+    target_pair: str,
+    recent_returns: int
+) -> pd.DataFrame:
+    forex_features_df = pd.DataFrame({'<DT>' : pd.Series(dtype=FOREX_COLS['<DT>'])})
+
+    for pair in data:
+        pair_df = data[pair]
+        pair_df.drop(['<HIGH>', '<LOW>'], axis=1, inplace=True)
+
+        if pair != target_pair:
+            pair_df.drop(['<OPEN>'], axis=1, inplace=True)
+        else:
+            pair_df.rename(columns={'<OPEN>' : f'<{pair} OPEN>'}, inplace=True)
+
+        pair_df.rename(columns={'<CLOSE>' : f'<{pair} CLOSE>'}, inplace=True)
+
+        forex_features_df = forex_features_df.merge(pair_df, on='<DT>', how='outer', suffixes=[None, None])
+    
+    na_dt = forex_features_df[pd.isnull(forex_features_df).any(axis=1)]['<DT>']
+    forex_features_df = forex_features_df[forex_features_df['<DT>'] > na_dt.iloc[-1]].reset_index(drop=True)
+
+    close_price_cols = [col for col in forex_features_df.columns if 'CLOSE' in col]
+
+    forex_features_df['<MIN SIN>'] = np.sin(2 * np.pi * forex_features_df['<DT>'].dt.minute / 60).astype(np.float32)
+    forex_features_df['<HOUR SIN>'] = np.sin(2 * np.pi * forex_features_df['<DT>'].dt.hour / 24).astype(np.float32)
+    forex_features_df['<WEEKDAY SIN>'] = np.sin(2 * np.pi * forex_features_df['<DT>'].dt.weekday / 7).astype(np.float32)
+
+    for close_price_col in close_price_cols:
+    
+        log_returns = np.log(forex_features_df[close_price_col] / forex_features_df[close_price_col].shift(1))
+        for i in range(0, recent_returns):
+            forex_features_df[f'<{close_price_col.strip("<>")} RECENT RETURN {i+1}>'] = log_returns.shift(i)
+
+        if target_pair not in close_price_col:
+            forex_features_df.drop(close_price_col, axis=1, inplace=True)
+    
+    forex_features_df.dropna(inplace=True)
+    forex_features_df.reset_index(drop=True, inplace=True)
+
+    return forex_features_df
+
+
+
+
