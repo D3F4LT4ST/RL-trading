a181537b535cab3e41336faa2ad3bc196ca781bc
Andrew Stepura <andstepura@gmail.com>
2023-01-03 01:54:58 -0500

    Added default hyperparameters for DQN, A2C and PPO

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 DATA_PATH = os.path.abspath(os.path.realpath(__file__) + '../../data')
 LOGS_PATH = os.path.abspath(os.path.realpath(__file__) + '../../logs')
+HYPERPARAMS_PATH = os.path.abspath(os.path.realpath(__file__) + '../../hyperparams')
 
 
